541020c9da9c3340b3c8a37975202bc5c9b1bc7b
shiny <shinyling123400@163.com>
2019-05-14 17:24:00 +0800

    加入nacos 应配配置文件。添加gateway(未完成，报错)

---
+++ cloud-gateway/src/main/java/com/everwing/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.everwing.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DELL shiny
+ * @create 2019/5/14
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
