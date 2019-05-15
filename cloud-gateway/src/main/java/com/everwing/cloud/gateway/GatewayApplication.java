8fce3261332eed97161723c4aca07b97389f549f
shiny <shinyling123400@163.com>
2019-05-15 11:26:56 +0800

    gateway 服务名调用。lb调用成功(admin暂时不能通过gateway调用存在跨域问题)

--- cloud-gateway/src/main/java/com/everwing/cloud/gateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/com/everwing/cloud/gateway/GatewayApplication.java
@@ -2,12 +2,14 @@ package com.everwing.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author DELL shiny
  * @create 2019/5/14
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
