92811eb42b6fd536cc986d1080b2df28a82a2f28
shiny <shinyling123400@163.com>
2019-05-24 17:51:06 +0800

    修改gateway至zuul

--- cloud-gateway/src/main/java/com/everwing/cloud/gateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/com/everwing/cloud/gateway/GatewayApplication.java
@@ -1,13 +1,18 @@
 package com.everwing.cloud.gateway;
 
+import com.everwing.cloud.gateway.filter.AccessFilter;
+import com.everwing.cloud.gateway.filter.ErrorFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author DELL shiny
  * @create 2019/5/14
  */
+@EnableZuulServer
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
@@ -15,4 +20,15 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
     }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+
 }
---
+++ cloud-gateway/src/main/java/com/everwing/cloud/gateway/filter/ErrorFilter.java
@@ -0,0 +1,37 @@
+package com.everwing.cloud.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class ErrorFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "error";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        Throwable throwable = ctx.getThrowable();
+        log.error("this is a ErrorFilter : {}", throwable.getCause().getMessage());
+        ctx.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        ctx.set("error.exception", throwable.getCause());
+        return null;
+    }
+
+}
